72e5f6fbf673a80afa43824ef500e0a485e9408b
eryvia <eryvia.reborn@gmail.com>
2025-10-23 23:15:01 +0200

    init 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,5 +9,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express + TypeScript!");
 });
 
+app.get("/status", (req: Request, res: Response) => {
+  res.json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
